63e8e5f2d0c51e783b9428fb11e121f0b12d0b79
Prashani <aruniprashani@gmail.com>
2020-04-17 12:45:50 +0530

    Implemented code for database connection

--- PaymentAPI/src/main/java/com/ZTestNew/model/Payment.java
+++ PaymentAPI/src/main/java/com/ZTestNew/model/Payment.java
@@ -17,5 +17,18 @@ import com.google.gson.JsonParser;
 
 public class Payment {
 	
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sliitpafs", "root", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+
 
 }

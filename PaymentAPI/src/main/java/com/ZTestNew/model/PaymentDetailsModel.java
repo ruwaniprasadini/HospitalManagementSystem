c718d6489604a51c7e79d1187f01a32c717cc013
Prashani <aruniprashani@gmail.com>
2020-04-17 12:40:19 +0530

    Implemented PaymentDetailsModel class

--- PaymentAPI/src/main/java/com/ZTestNew/model/PaymentDetailsModel.java
+++ PaymentAPI/src/main/java/com/ZTestNew/model/PaymentDetailsModel.java
@@ -3,6 +3,55 @@ package com.ZTestNew.model;
 public class PaymentDetailsModel {
 
 	
+	private int ppymentDetailsId;
+	private String cardNo;
+	private String cvv;
+	private String patientID;
+	
+	
+	public PaymentDetailsModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getPpymentDetailsId() {
+		return ppymentDetailsId;
+	}
+
+
+	public void setPpymentDetailsId(int ppymentDetailsId) {
+		this.ppymentDetailsId = ppymentDetailsId;
+	}
+
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+
+	public String getCvv() {
+		return cvv;
+	}
+
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+
+
+	public String getPatientID() {
+		return patientID;
+	}
+
+
+	public void setPatientID(String patientID) {
+		this.patientID = patientID;
+	}
 	
 	
 
